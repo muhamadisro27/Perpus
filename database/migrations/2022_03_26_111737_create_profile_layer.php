bec397ac9082a86052ae916cdcff3236c7164497
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-26 19:42:00 +0800

    feat : setup migration for profile

---
+++ database/migrations/2022_03_26_111737_create_profile_layer.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('avatar');
+            $table->string('birth_place');
+            $table->date('birth_date');
+            $table->boolean('sex');
+            $table->string('religion');
+            $table->text('address');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('profile_id');
+            $table->bigInteger('grade_id');
+            $table->bigInteger('major_id');
+            $table->string('nim');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('profile_id');
+            $table->bigInteger('major_id');
+            $table->string('nip');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+        Schema::dropIfExists('students');
+        Schema::dropIfExists('employees');
+    }
+};
