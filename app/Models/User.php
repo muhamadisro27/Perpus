a137644afd8def504bb029bad33e8b912b1d4d26
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-26 19:42:39 +0800

    feat : add relation profile on model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'profile_id',
         'email',
         'password',
     ];
@@ -41,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class, 'profile_id');
+    }
 }
