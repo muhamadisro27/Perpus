c2e0d4c29033c650931ad31cf21da095937ea7da
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-30 19:41:25 +0800

    feat : change field name to username

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'profile_id',
+        'username',
         'email',
         'password',
     ];
