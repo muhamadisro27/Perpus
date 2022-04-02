d72193bef5239d6406f4332d39853e3998c6d334
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-02 21:03:19 +0800

    feat : fix user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Profile;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -16,9 +17,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        Profile::factory(20)->create()->each(function ($profile) {
-            $user= User::factory()->make();
-            $profile->user()->save($user);
-        });
+        if(App::environment(['local','testing','production']))
+        {
+            // create profile & users    
+            $this->command->info('Initial User');
+            $this->command->getOutput()->progressStart(20);
+            Profile::factory(20)->create()->each(function ($profile) {
+                $user= User::factory()->make();
+                $profile->user()->save($user);
+            });
+            $this->command->getOutput()->progressFinish();
+     
+        } else {
+            $this->command->info('Skip');
+        }
     }
 }
