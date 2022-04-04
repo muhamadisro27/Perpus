8025285392eb35839ec9e52da457d6bbc665d4ca
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-05 06:40:19 +0800

    feat : build interface for update book

--- app/Http/Interfaces/Book/BookInterface.php
+++ app/Http/Interfaces/Book/BookInterface.php
@@ -6,7 +6,7 @@ interface BookInterface {
    public function index();
    // public function show();
    public function store($data);
-   // public function update();
+   public function update($data,$book);
    public function destroy($data);
 }
 
