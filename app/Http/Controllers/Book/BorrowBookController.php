770d2194fd491fceb0ca3ca586fbae82ce4ad3da
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-05 11:09:20 +0800

    feat : add controller borrowbook

---
+++ app/Http/Controllers/Book/BorrowBookController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Book;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BorrowBookController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function borrowBooks(Book $book)
+    {
+        
+    }
+}
