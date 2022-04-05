89093895c023c32b2c9bafb01c2e7baf2c331556
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-05 16:43:56 +0800

    feat : call student seeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,24 @@ class DatabaseSeeder extends Seeder
          
          $this->call(UserSeeder::class);
 
+         // Initial Default System
+
+         $this->command->info(PHP_EOL);
+         $this->command->info('⚙️ initial'.PHP_EOL);
+         $this->command->info('********** Initial Default System **********'.PHP_EOL);
+         $this->command->info('************************************'.PHP_EOL);
+         
+         $this->call(DefaultMasterSeeder::class);
+
+          // Initial Student Activity
+
+          $this->command->info(PHP_EOL);
+          $this->command->info('⚙️ initial'.PHP_EOL);
+          $this->command->info('********** Initial Student **********'.PHP_EOL);
+          $this->command->info('************************************'.PHP_EOL);
+          
+          $this->call(StudentSeeder::class);
+
          // Initial Book Activity
 
          $this->command->info(PHP_EOL);
